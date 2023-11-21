d65eaffa9bd222749f391d9e0728110636cdead4
Owen McMonagle <8026115+RikuKarasuma@users.noreply.github.com>
2023-11-21 23:33:44 +0000

    #2

--- Solution.java
+++ Solution.java
@@ -1,14 +1,14 @@
 class Solution {
 
-	/*
-	* Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray
-	* whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
-	* 
-	* Example 1:
+   /*
+    * Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray
+    * whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+    * 
+    * Example 1:
     * Input: target = 7, nums = [2,3,1,2,4,3]
     * Output: 2
     * Explanation: The subarray [4,3] has the minimal length under the problem constraint.
-	*
+    *
     * Example 2:
     * 
     * Input: target = 4, nums = [1,4,4]
@@ -17,7 +17,7 @@ class Solution {
     * 
     * Input: target = 11, nums = [1,1,1,1,1,1,1,1]
     * Output: 0
-	*/
+    */
     public int minSubArrayLen(int target, int[] nums) {
 
         if (target < 1)
